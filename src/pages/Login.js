0daf41aa038aa8298de22b4c2990aa128b4c206c
ChoiWonYu <phillip07290@naver.com>
2022-08-11 22:09:34 +0900

    feat : login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,14 +6,29 @@ const Login = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
-
+  const onValid = (data) => {
+    alert("로그인 되었습니다.");
+  };
+  const onInValid = () => {};
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onValid, onInValid)}>
         <p>ID</p>
-        <input type="ID" {...register("ID")}></input>
+        <input
+          placeholder="이메일을 입력해주세요"
+          type="ID"
+          {...register("ID", {
+            pattern: {
+              value:
+                /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
+              message: "이메일 형식에 맞게 입력해주세요.",
+            },
+          })}
+        ></input>
+        {errors.ID && <div>{errors.ID.message}</div>}
         <p>PW</p>
         <input type="password" {...register("PW")}></input>
+        <button>LogIn</button>
       </form>
     </div>
   );
